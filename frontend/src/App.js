345199a4b1856525e08bfe147d0e20aac7f98c28
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2023-04-02 07:36:18 +0530

    Front End Started

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './components/Home';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
